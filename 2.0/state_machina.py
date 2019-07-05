de4dedc945cd885c1e393e9d9a574cd236dba581
TylersDurden <srobbin1@ithaca.edu>
2019-07-04 22:51:22 -0400

    automata

--- 2.0/state_machina.py
+++ 2.0/state_machina.py
@@ -55,11 +55,16 @@ def big_bang(state, n_steps, doSave):
         depth = np.array(state).max()
         for x in range(state.shape[0]):
             for y in range(state.shape[1]):
-                if world[x, y] % 5 and world[x,y] > avg:
+                if world[x, y] % 3 and world[x, y] <= avg:
                     state[x, y] -= 1
-                if (world[x, y] == 8 or world[x,y]%4==0) and state[x,y] <=1:
-                    state[x,y] += 1
-                    world[x,y] += 1
+                    world[x, y] = 1
+                if world[x, y] == 5 and state[x, y] >=1:
+                    state[x, y] += 1
+                    world[x, y] = 1
+                if (state[x, y] == 0 or state[x,y]==2) and world[x, y] % 7 == 0:
+                    state[x, y] = 1
+                if np.abs(state[x, y]) == depth and world[x, y] != 8:
+                    state[x, y] -= 1
         simulation.append([plt.imshow(state, 'gray')])
         history.append(state)
     print '\033[31m\033[1m\t\t** SIMULATION FINISHED [%ss Elapsed]\033[0m\033[1m **\033[0m' % \
@@ -73,31 +78,40 @@ def big_bang(state, n_steps, doSave):
     return history
 
 
-''' DEFINE DIMENISONS'''
-w = 250
-h = 250
-state = np.zeros((w, h))
-''' CREATE INITIAL STATE '''
-state = imutils.draw_centered_circle(state, 120, 1, False)
-state = imutils.draw_centered_circle(state, 60, 2, False)
-state = imutils.draw_centered_circle(state, 20, 1, False)
-state = imutils.draw_centered_box(state, 80, 0,False)
-state = imutils.draw_centered_circle(state,40,1, True)
-plt.imshow(state, 'gray')
-plt.close()
+def main():
+    ''' DEFINE DIMENISONS'''
+    w = 250
+    h = 250
+    state = np.zeros((w, h))
+    ''' CREATE INITIAL STATE '''
+    state = imutils.draw_centered_circle(state, 120, 2, False)
+    state = imutils.draw_centered_circle(state, 60, 2, False)
+    state = imutils.draw_centered_box(state, 20, 0, False)
+    state = imutils.draw_centered_box(state, 80, 1, False)
+    state = imutils.draw_centered_circle(state, 40, 2, True)
 
-if len(sys.argv)>1:
-    print '\033[1m\033[36mSTARTING ' + str(sys.argv[1]).upper() + ' SIMULATION\033[0m'
-    ''' SELECT A SIMULATION '''
-if 'growth' in sys.argv:
+    state_2 = np.zeros((w, h))
+    state_2 = imutils.draw_centered_box(state, 120, 2, False)
+    state_2 = imutils.draw_centered_circle(state, 80, 1, False)
 
-    save = {'do': True,
-            'name': 'growth.mp4',
-            'fps': 55}
-    sim = growth_simulate(state, 350, save)
-if 'big_bang' in sys.argv:
-    save = {'do': True,
-            'name': 'big_bang_4.mp4',
-            'fps': 65}
-    sim = big_bang(state, 150, save)
+    plt.imshow(state, 'gray')
+    plt.close()
+
+    if len(sys.argv) > 1:
+        print '\033[1m\033[36mSTARTING ' + str(sys.argv[1]).upper() + ' SIMULATION\033[0m'
+        ''' SELECT A SIMULATION '''
+    if 'growth' in sys.argv:
+        save = {'do': False,
+                'name': 'growth.mp4',
+                'fps': 55}
+        sim = growth_simulate(state, 350, save)
+    if 'big_bang' in sys.argv:
+        save = {'do': False,
+                'name': 'big_bang_5.mp4',
+                'fps': 65}
+        sim = big_bang(state, 150, save)
+
+
+if __name__ == '__main__':
+    main()
 
