5270ebc50c0217f86b8552b7473a88245ca0a240
TylersDurden <srobbin1@ithaca.edu>
2019-07-04 21:59:16 -0400

    automata

--- 2.0/state_machina.py
+++ 2.0/state_machina.py
@@ -12,22 +12,22 @@ def growth_simulate(state, n_steps, doSave):
     t0 = time.time()
     f = plt.figure()
     history = [state]
-    simulation = []
-    simulation.append([plt.imshow(state, 'gray_r')])
+    simulation = list()
+    simulation.append([plt.imshow(state, 'gray')])
     for i in range(n_steps):
         world = ndi.convolve(state, np.ones((3, 3)))
         avg = world.mean()
         for x in range(state.shape[0]):
             for y in range(state.shape[1]):
-                if world[x, y] % 5 and world[x,y] > avg:
+                if world[x, y] % 5 and world[x,y] >= avg:
                     state[x, y] = 1
-                if world[x, y] == 8 and state[x,y] <=1:
-                    state[x,y] = 0
                     world[x,y] = 1
-                if world[x, y] % 3 == 0:
+                if world[x, y] == 8 and state[x,y] == 0:
+                    state[x,y] = -1
+                    world[x,y] = 1
+                if world[x, y] % 3 == 0 and state[x,y]>=1:
                     state[x, y] -= 1
-                if world[x, y] % 4 == 0:
-                    state[x, y] += 1
+                    world[x,y] = 1
                 if state[x,y] > 15 and (world[x,y] == 8 or world[x,y] == 6):
                     state[x,y] = 0
         simulation.append([plt.imshow(state, 'gray')])
@@ -47,7 +47,7 @@ def big_bang(state, n_steps, doSave):
     f = plt.figure()
     t0 = time.time()
     history = [state]
-    simulation = []
+    simulation = list()
     simulation.append([plt.imshow(state, 'gray')])
     for i in range(n_steps):
         world = ndi.convolve(state, np.ones((3, 3)))
@@ -57,11 +57,9 @@ def big_bang(state, n_steps, doSave):
             for y in range(state.shape[1]):
                 if world[x, y] % 5 and world[x,y] > avg:
                     state[x, y] -= 1
-                if world[x, y] == 8 and state[x,y] <=1:
+                if (world[x, y] == 8 or world[x,y]%4==0) and state[x,y] <=1:
                     state[x,y] += 1
                     world[x,y] += 1
-                if state[x,y] > 15 and (world[x,y] == 8 or world[x,y] == 6):
-                    state[x,y] = 0
         simulation.append([plt.imshow(state, 'gray')])
         history.append(state)
     print '\033[31m\033[1m\t\t** SIMULATION FINISHED [%ss Elapsed]\033[0m\033[1m **\033[0m' % \
@@ -81,24 +79,25 @@ h = 250
 state = np.zeros((w, h))
 ''' CREATE INITIAL STATE '''
 state = imutils.draw_centered_circle(state, 120, 1, False)
-state = imutils.draw_centered_circle(state, 60, -1, False)
-state = imutils.draw_centered_circle(state, 20, 2, False)
-state = imutils.draw_centered_circle(state, 10, -1, False)
-state = imutils.draw_centered_box(state,80,2,False)
-state = imutils.draw_centered_box(state,40,-1,False)
+state = imutils.draw_centered_circle(state, 60, 2, False)
+state = imutils.draw_centered_circle(state, 20, 1, False)
+state = imutils.draw_centered_box(state, 80, 0,False)
+state = imutils.draw_centered_circle(state,40,1, True)
+plt.imshow(state, 'gray')
 plt.close()
 
-print '\033[1m\033[36mSTARTING ' + str(sys.argv[1]).upper()+' SIMULATION\033[0m'
-''' SELECT A SIMULATION '''
+if len(sys.argv)>1:
+    print '\033[1m\033[36mSTARTING ' + str(sys.argv[1]).upper() + ' SIMULATION\033[0m'
+    ''' SELECT A SIMULATION '''
 if 'growth' in sys.argv:
 
     save = {'do': True,
-            'name': 'big_bang_2.mp4',
-            'fps': 75}
-    simulation = growth_simulate(state, 350, save)
+            'name': 'growth.mp4',
+            'fps': 55}
+    sim = growth_simulate(state, 350, save)
 if 'big_bang' in sys.argv:
     save = {'do': True,
-            'name': 'big_bang_3.mp4',
+            'name': 'big_bang_4.mp4',
             'fps': 65}
-    simulation = big_bang(state, 150, save)
+    sim = big_bang(state, 150, save)
 
